d15f9df62a33d1098df0d7d66429c875a8e6af85
Gustavonvp <gustavonvp@gmail.com>
2022-01-07 07:02:50 -0300

    foEach no servidor com pages EJS

--- server.js
+++ server.js
@@ -5,7 +5,20 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-    res.render("pages/index");
+    const items = [
+        {
+            title: "D",
+            mensagem: "Desenvolver aplicações/serviços de forma fácil",
+        },
+        {
+            title: "E",
+            mensagem: "EJS usa JavaScript para renderizar HTML",
+        }
+    ]
+
+    res.render("pages/index", {
+        qualitys: items,
+    });
 })
 
 app.get("/sobre", function (req,res){
